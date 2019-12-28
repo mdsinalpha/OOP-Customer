6ba1fcf7dc09f813b7e3cecd64c354f4e1a7fa7c
smrz <mr.razavi77@gmail.com>
2019-12-29 03:04:37 +0330

    register activity added

---
+++ src/main/java/oop/customer/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package oop.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import oop.customer.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
