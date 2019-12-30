f29045214dcb71549cf9966a217c55ed245003b2
alibashari <mabm13770@gmail.com>
2019-12-30 18:20:55 +0330

    add product activity

---
+++ src/main/java/oop/customer/ProductPageActivity.kt
@@ -0,0 +1,12 @@
+package oop.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_page)
+    }
+}
